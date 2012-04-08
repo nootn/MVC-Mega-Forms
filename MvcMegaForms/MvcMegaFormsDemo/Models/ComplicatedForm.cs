6376989cd79841ea583b210689a182945f1db809
Andrew Newton <nootn@hotmail.com>
2012-04-08 21:49:30 +1000

    Adds MVC Foolproof with a couple of samples.
    https://trello.com/card/create-form-in-mvcmegaformsdemo-using-mvc-foolproof-validation-with-multiple-scenarios-to-investigate-overlap/4f80dfdad096c0a82e10b37f/8

--- MvcMegaForms/MvcMegaFormsDemo/Models/ComplicatedForm.cs
+++ MvcMegaForms/MvcMegaFormsDemo/Models/ComplicatedForm.cs
@@ -1,13 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using Foolproof;
 
 namespace MvcMegaFormsDemo.Models
 {
     public class ComplicatedForm
     {
+        [HiddenInput]
         public int ComplicatedFormId { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string EqualToInitial { get; set; }
+
+        [EqualTo("EqualToInitial", ErrorMessage = "EqualToSecond must be equal to EqualToInitial.")]
+        public string EqualToSecond { get; set; }
+
+        //TODO: Enable this is EqualToInitial == EqualToSecond!
+        public string EqualToShowIfOk { get; set; }
+
+        public bool ShowNextField { get; set; }
+
+        //TODO: show this if ShowNextField == true
+        [RequiredIf("ShowNextField", Operator.EqualTo, true)]
+        public string NextField { get; set; }
     }
 }
