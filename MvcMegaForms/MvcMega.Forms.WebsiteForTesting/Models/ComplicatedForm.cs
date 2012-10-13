2a468994b28259b4814aebd3e08687f7fa717ef6
Andrew Newton <nootn@hotmail.com>
2012-10-13 15:05:22 +1000

    Adds example of how to get input masking working with detect form changes.
    https://www.trello.com/card/detectchanges-need-to-support-not-saying-change-when-mask-applied/4f80dfdad096c0a82e10b37f/37

--- MvcMegaForms/MvcMega.Forms.WebsiteForTesting/Models/ComplicatedForm.cs
+++ MvcMegaForms/MvcMega.Forms.WebsiteForTesting/Models/ComplicatedForm.cs
@@ -8,6 +8,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
@@ -22,6 +23,8 @@ namespace MvcMega.Forms.WebsiteForTesting.Models
         {
             PopulateAllTestDropDownItems();
             //TestSelectedDropDownItemId = 1; //this is for the detect form changes..
+
+            TimeOfDayForMaskTesting = new TimeSpan(14, 15, 0);
         }
 
         public void PopulateAllTestDropDownItems()
@@ -140,5 +143,7 @@ namespace MvcMega.Forms.WebsiteForTesting.Models
         public SubDetails FirstSubDetails { get; set; }
 
         public SubDetails SecondSubDetails { get; set; }
+
+        public TimeSpan TimeOfDayForMaskTesting { get; set; }
     }
 }
